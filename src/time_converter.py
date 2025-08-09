42280de90bc66e49b6a9f1f8d932a21294dd93ed
QKiryuIsASlowDumbLoser <qkiryuosupad@gmail.com>
2025-08-09 12:22:40 -0500

    initial commit

---
+++ src/time_converter.py
@@ -0,0 +1,57 @@
+from fractions import Fraction
+
+class _TimeProperty:
+    class __In:
+        def __init__(self, seconds_in_property):
+            self.__Second= seconds_in_property
+            self.__Minute= self.__Second * 60
+            self.__Hour  = self.__Minute * 60 
+            self.__Day   = self.__Hour * 24
+            self.__Week  = self.__Day * 7
+         
+        def __Seconds(self, quantity: float)-> float:
+            return float(self.__Second * quantity) 
+
+        @property
+        def Seconds(self):
+            return self.__Seconds
+        
+        def __Minutes(self, quantity: float)-> float:
+            return float(self.__Minute * quantity)
+
+        @property
+        def Minutes(self):
+            return self.__Minutes
+        
+        def __Hours(self, quantity: float)-> float:
+            return float(self.__Hour * quantity)
+
+        @property
+        def Hours(self):
+            return self.__Hours
+        
+        def __Days(self, quantity: float)-> float:
+            return float(self.__Day * quantity)
+
+        @property
+        def Days(self):
+            return self.__Days
+        
+        def __Weeks(self, quantity: float)-> float:
+            return float(self.__Week * quantity)
+
+        @property
+        def Weeks(self):
+            return self.__Weeks
+        
+    
+         
+    def __init__(self, seconds_in_property):
+        self.In= _TimeProperty.__In(seconds_in_property)
+
+Seconds= _TimeProperty(1)
+Minutes= _TimeProperty(Fraction(1, int(Seconds.In.Minutes(1))))
+Hours  = _TimeProperty(Fraction(1, int(Seconds.In.Hours(1))))
+Days   = _TimeProperty(Fraction(1, int(Seconds.In.Days(1))))
+Weeks  = _TimeProperty(Fraction(1, int(Seconds.In.Weeks(1))))
+
